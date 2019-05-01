7ffffea99151c1a9705fa1a92d1795c293d6aac3
Lenny <lenbencherif@gmail.com>
2019-05-01 10:18:35 +0200

    Detect trigger for each floor

---
+++ wfc_zelda/WFC_Zelda/Assets/detectTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectTrigger : MonoBehaviour
+{
+  public GameObject Quad;
+  public float floor;
+  public bool isFirst;
+    // Start is called before the first frame update
+    void Start()
+    {
+    Quad.transform.position = new Vector3(0, 50, 0);
+
+  }
+
+  // Update is called once per frame
+  void Update()
+    {
+        
+    }
+
+  void modifyQuad()
+  {
+  }
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.tag == "Player") {
+      Debug.Log("TEst");
+      Quad.transform.position = new Vector3(0, floor, 0);
+      if (isFirst) {
+        GameObject.Find("VILLAGE").SetActive(false);
+
+      }
+      //Quad.transform.Translate(1, floor, 1);
+    }
+  }
+}
